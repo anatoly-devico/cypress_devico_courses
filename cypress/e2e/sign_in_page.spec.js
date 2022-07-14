cf7b75f8aa58aa76538fc0aed273dda45e79eec3
Anatoly_devico <anatolii.husakovskyi@devico.io>
2022-07-14 19:47:36 +0300

    initial commit

---
+++ cypress/e2e/sign_in_page.spec.js
@@ -0,0 +1,16 @@
+import {sign_in_page} from "../selectors/sign_in_page";
+
+describe('UI tests for sign in page', () => {
+
+  before('visiting sign in page', () => {
+    cy.visit('/')
+  })
+
+  it('should show "Real World App logo"', () => {
+    cy.get(sign_in_page.logo_image).should('be.visible').and('have.attr', 'xmlns', 'http://www.w3.org/2000/svg')
+  })
+
+  it('should show "Sign in" title', () => {
+    cy.get(sign_in_page.title_text).should('be.visible').and('have.text', 'Sign in')
+  })
+})
