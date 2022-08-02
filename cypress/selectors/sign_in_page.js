2ad86f83f60b853a33e3fbd164e75d8a2b19522d
Anatoly_devico <anatolii.husakovskyi@devico.io>
2022-08-02 21:09:51 +0300

    homework 2.8

--- cypress/selectors/sign_in_page.js
+++ cypress/selectors/sign_in_page.js
@@ -1,7 +1,15 @@
 export const sign_in_page = {
-  logo_image: '.makeStyles-logo-3',
-  title_text: '.MuiTypography-h5',
-  get_selector_for_date(date){
-    return `date today ${date}`
-  }
-}
+  logo_image: ".makeStyles-logo-3",
+  title_text: ".MuiTypography-h5",
+  username_field: "#username",
+  password_field: "#password",
+  username_title: "#username-label",
+  password_title: "#password-label",
+  username_validation_message: "#username-helper-text",
+  password_validation_message: "#password-helper-text",
+  rememberMe_checkbox: ".PrivateSwitchBase-input-14",
+  signIn_button: '[data-test="signin-submit"]',
+  signUp_link: '[data-test="signup"]',
+  cypress_copyright: '[rel="noopener noreferrer"]',
+  signIn_error: '[data-test="signin-error"]',
+};
